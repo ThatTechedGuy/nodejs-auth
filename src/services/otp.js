cd215ed1a9214808c4121ed3693409be70dbbce6
ThatTechedGuy <vvishalgupta5@gmail.com>
2020-04-10 16:26:32 +0530

    clean up

--- src/services/otp.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto-random-string';
-
-// Generates random 6 digit OTP
-const generateOTP = () => {
-  return crypto({ length: 6, type: 'distinguishable' });
-};
-
-export default generateOTP;
